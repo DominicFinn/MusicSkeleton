eba2da70599449a97f60c8393b5e2ba16ca036c0
DominicFinn <dom.finn@gmail.com>
2013-09-06 21:16:55 +0100

    Source code added

---
+++ src/Music.Core/SineWaveProvider.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Music.Core
+{
+    public sealed class SineWaveProvider : WaveProvider
+    {
+        int sample;
+
+        public SineWaveProvider()
+        {
+            Frequency = 600;
+            Amplitude = 0.25f;           
+        }
+
+        public float Frequency { get; set; }
+        public float Amplitude { get; set; }
+
+        public override int Read(float[] buffer, int offset, int sampleCount)
+        {
+            int sampleRate = WaveFormat.SampleRate;
+            for (int n = 0; n < sampleCount; n++)
+            {
+                buffer[n + offset] = (float)(Amplitude * Math.Sin((2 * Math.PI * sample * Frequency) / sampleRate));
+                sample++;
+                if (sample >= sampleRate) sample = 0;
+            }
+            return sampleCount;
+        }
+    }
+}
---
+++ src/Music.Core/WaveProvider.cs
@@ -0,0 +1,34 @@
+﻿using NAudio.Wave;
+
+namespace Music.Core
+{
+    public abstract class WaveProvider : IWaveProvider
+    {
+        protected WaveProvider()
+            : this(44100, 1)
+        {
+        }
+
+        public WaveFormat WaveFormat { get; private set; }
+
+        protected WaveProvider(int sampleRate, int channels)
+        {
+            SetWaveFormat(sampleRate, channels);
+        }
+
+        public void SetWaveFormat(int sampleRate, int channels)
+        {
+            this.WaveFormat = WaveFormat.CreateIeeeFloatWaveFormat(sampleRate, channels);
+        }
+
+        public int Read(byte[] buffer, int offset, int count)
+        {
+            var waveBuffer = new WaveBuffer(buffer);
+            var samplesRequired = count / 4;
+            var samplesRead = Read(waveBuffer.FloatBuffer, offset / 4, samplesRequired);
+            return samplesRead * 4;
+        }
+
+        public abstract int Read(float[] buffer, int offset, int sampleCount);
+    }
+}
